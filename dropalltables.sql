b5e070305729fd88c6f9e0809dba47c6a09e12de
bretlowery <bret.lowery@pikktu.com>
2016-02-17 23:36:44 -0500

    openssl encryption

---
+++ dropalltables.sql
@@ -0,0 +1,14 @@
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS auth_group;
+DROP TABLE IF EXISTS auth_group_permissions;
+DROP TABLE IF EXISTS auth_permission;
+DROP TABLE IF EXISTS auth_user;
+DROP TABLE IF EXISTS auth_user_groups;
+DROP TABLE IF EXISTS auth_user_user_permissions;
+DROP TABLE IF EXISTS django_admin_log;
+DROP TABLE IF EXISTS django_content_type;
+DROP TABLE IF EXISTS django_session;
+DROP TABLE IF EXISTS snakr_longurls;
+DROP TABLE IF EXISTS snakr_shorturls;
+DROP TABLE IF EXISTS snakr_snakrlog;
+SET FOREIGN_KEY_CHECKS = 1;
